7a145d32c0c17dc077c2cc99c7dd7541e862c934
Psykies <32191652+Psykies@users.noreply.github.com>
2023-04-19 10:36:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ navigator.mediaDevices.getUserMedia({ video: true })
 const robot1Btn = document.getElementById("robot1-btn");
 const robot2Btn = document.getElementById("robot2-btn");
 
-const card = document.querySelector(".card");
-
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector(".card2");
 
 
 
 // Add event listener to toggle button
 robot1Btn.addEventListener("click", function() {
   // Toggle the display of the card
-  if (card.style.display === "none") {
-    card.style.display = "block";
+  if (card1.style.display === "none") {
+    card1.style.display = "block";
   } else {
-    card.style.display = "none";
+    card1.style.display = "none";
   }
 });
 
 // Function to update the values in the table
-function updateValues() {
+function updateValues1() {
   // Generate random values for x and y
   var x_robot1 = Math.floor(Math.random() * 100) + 1;
   var y_robot1 = Math.floor(Math.random() * 100) + 1;
@@ -46,6 +46,40 @@ function updateValues() {
 }
 
 // Update the values every second
-setInterval(updateValues, 1000);
+setInterval(updateValues1, 1000);
+
+
+// Add event listener to toggle button
+robot2Btn.addEventListener("click", function() {
+    // Toggle the display of the card
+    if (card2.style.display === "none") {
+      card2.style.display = "block";
+    } else {
+      card2.style.display = "none";
+    }
+  });
+
+  // Function to update the values in the table
+function updateValues2() {
+    // Generate random values for x and y
+    var x_robot2 = Math.floor(Math.random() * 100) + 1;
+    var y_robot2 = Math.floor(Math.random() * 100) + 1;
+    var name_robot2 = "Robot 2";
+    var battery_robot2 = 100 + "%"
+      
+  
+    // Update the values displayed on the page
+    document.getElementById("x-value2").textContent = x_robot2;
+    document.getElementById("name2").textContent = name_robot2;
+    document.getElementById("y-value2").textContent = y_robot2;
+    document.getElementById("Battery2").textContent = battery_robot2;
+  }
+
+// Update the values every second
+setInterval(updateValues2, 1000);
+
+
+  
+
 
 
