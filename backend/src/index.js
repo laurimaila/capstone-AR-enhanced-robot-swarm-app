57ad926e38546584774dedf645b33adedd77a4b1
Lauri Maila <laurikmaila@gmail.com>
2023-05-14 21:39:13 +0300

    Hardcoded ROS robot listeners

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-
+const util = require('./util');
 const express = require('express');
 const ROSLIB = require('roslib');
 const bodyParser = require('body-parser');
@@ -8,25 +8,29 @@ const morgan = require('morgan');
 // For streamer fs and path
 const fs = require('fs');
 const path = require('path');
-
-
-const WEBSOCKET_PORT = 3005;
-
-const ros = new ROSLIB.Ros({ url: `ws://localhost:${WEBSOCKET_PORT}` });
+const { connected } = require('process');
 
 var robotData = [
-    {},
-    {}
-]
+    null, null, null, null, null,
+    null, null, null, null, null
+];
 
 var topicList = [];
-
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+const WEBSOCKET_PORT = 9090;
+
+const ros = new ROSLIB.Ros({ url: `ws://localhost:${WEBSOCKET_PORT}` });
+
+getTopics().then(response => {
+    topicList = response;
+    console.log("List of topics:", topicList);
+});
 
 function getTopics() {
     return new Promise((resolve, reject) => {
@@ -51,35 +55,30 @@ function getTopics() {
             }
         );
     });
-}
-
-
-
-getTopics().then(response => {
-    topicList = response;
-});
-
-console.log(topicList)
-
-
+};
 
 app.get('/', (req, res) => {
-    console.log("topicit", topicsList)
-    res.send(turtledata);
+    res.send(robotData);
 });
 
-app.get('/turtle3', (req, res) => {
-    res.send(turtledata[0]);
+app.get('/1', (req, res) => {
+    res.send(robotData[0]);
 });
 
-app.get('/turtle4', (req, res) => {
-    res.send(turtledata[1]);
+app.get('/2', (req, res) => {
+    res.send(robotData[1]);
 });
 
-app.get('/distance', (req, res) => {
-    let distance = Math.sqrt((turtledata[1].pose.position.x - turtledata[0].pose.position.x) ** 2
-        + (turtledata[1].pose.position.y - turtledata[0].pose.position.y) ** 2);
-    res.send(JSON.stringify(distance));
+app.get('/3', (req, res) => {
+    res.send(robotData[2]);
+});
+
+app.get('/4', (req, res) => {
+    res.send(robotData[3]);
+});
+
+app.get('/5', (req, res) => {
+    res.send(robotData[4]);
 });
 
 ros.on("connection", () => {
@@ -87,11 +86,29 @@ ros.on("connection", () => {
 });
 
 ros.on("error", (error) => {
-    console.log(`errored out ${error}`);
+    console.log(`Error connecting to rosbridge server: ${error}`);
+    setTimeout(() => {
+        ros.connect(`ws://localhost:${WEBSOCKET_PORT}`);
+    }, "5000");
 });
 
 ros.on("close", () => {
     console.log("Disconnected from rosbridge server.");
+    setTimeout(() => {
+        ros.connect(`ws://localhost:${WEBSOCKET_PORT}`);
+    }, "5000");
+});
+
+const turtle1listener = new ROSLIB.Topic({
+    ros,
+    name: "/vrpn_client_node/turtle1/pose",
+    messageType: "geometry_msgs/msg/PoseStamped",
+});
+
+const turtle2listener = new ROSLIB.Topic({
+    ros,
+    name: "/vrpn_client_node/turtle2/pose",
+    messageType: "geometry_msgs/msg/PoseStamped",
 });
 
 const turtle3listener = new ROSLIB.Topic({
@@ -100,8 +117,6 @@ const turtle3listener = new ROSLIB.Topic({
     messageType: "geometry_msgs/msg/PoseStamped",
 });
 
-
-
 const turtle4listener = new ROSLIB.Topic({
     ros,
     name: "/vrpn_client_node/turtle4/pose",
@@ -109,15 +124,32 @@ const turtle4listener = new ROSLIB.Topic({
 });
 
 
+const turtle5listener = new ROSLIB.Topic({
+    ros,
+    name: "/vrpn_client_node/turtle5/pose",
+    messageType: "geometry_msgs/msg/PoseStamped",
+});
 
 
-turtle3listener.subscribe((message) => {
-    turtledata[0] = message;
+turtle1listener.subscribe((message) => {
+    robotData[0] = message;
 
 });
 
+turtle2listener.subscribe((message) => {
+    robotData[1] = message;
+});
+
+turtle3listener.subscribe((message) => {
+    robotData[2] = message;
+});
+
 turtle4listener.subscribe((message) => {
-    turtledata[1] = message;
+    robotData[3] = message;
+});
+
+turtle5listener.subscribe((message) => {
+    robotData[4] = message;
 });
 
 app.listen(3000, () => {
@@ -135,3 +167,15 @@ streamer.get('/video', (req, res) => {
 streamer.listen(4000, () => {
     console.log('Video: Listening on port 4000')
 });
+
+module.exports = {
+
+}
+
+/*
+app.get('/distance', (req, res) => {
+    let distance = Math.sqrt((turtledata[1].pose.position.x - turtledata[0].pose.position.x) ** 2
+        + (turtledata[1].pose.position.y - turtledata[0].pose.position.y) ** 2);
+    res.send(JSON.stringify(distance));
+});
+*/
